d80604ab660abe3f147d5e1384658a3303e51ce3
cjwd <chinarakinda@gmail.com>
2020-04-09 08:21:09 -0400

    Disable deenqueuing of woocommerce styles and scripts on non-woocommerce pages

--- core/boot/functions-assets.php
+++ core/boot/functions-assets.php
@@ -24,35 +24,35 @@ add_action('wp_enqueue_scripts', function() {
 /**
  * Remove the Javascript and CSS files loaded on non-woocommerce pages
  */
-add_action('wp_enqueue_scripts', function() {
-  if(function_exists('is_woocommerce')) {
-    if(!is_woocommerce() && !is_cart() && !is_checkout()) {
-      // Remove unnecessary stylesheets.
-      wp_dequeue_style( 'woocommerce-general' );
-      wp_dequeue_style( 'woocommerce-layout' );
-      wp_dequeue_style( 'woocommerce-smallscreen' );
-      wp_dequeue_style( 'woocommerce_frontend_styles' );
-      wp_dequeue_style( 'woocommerce_fancybox_styles' );
-      wp_dequeue_style( 'woocommerce_chosen_styles' );
-      wp_dequeue_style( 'woocommerce_prettyPhoto_css' );
+// add_action('wp_enqueue_scripts', function() {
+//   if(function_exists('is_woocommerce')) {
+//     if(!is_woocommerce() && !is_cart() && !is_checkout()) {
+//       // Remove unnecessary stylesheets.
+//       wp_dequeue_style( 'woocommerce-general' );
+//       wp_dequeue_style( 'woocommerce-layout' );
+//       wp_dequeue_style( 'woocommerce-smallscreen' );
+//       wp_dequeue_style( 'woocommerce_frontend_styles' );
+//       wp_dequeue_style( 'woocommerce_fancybox_styles' );
+//       wp_dequeue_style( 'woocommerce_chosen_styles' );
+//       wp_dequeue_style( 'woocommerce_prettyPhoto_css' );
 
-      // Remove unnecessary scripts.
-      wp_dequeue_script( 'wc_price_slider' );
-      wp_dequeue_script( 'wc-single-product' );
-      wp_dequeue_script( 'wc-add-to-cart' );
-      wp_dequeue_script( 'wc-cart-fragments' );
-      wp_dequeue_script( 'wc-checkout' );
-      wp_dequeue_script( 'wc-add-to-cart-variation' );
-      wp_dequeue_script( 'wc-single-product' );
-      wp_dequeue_script( 'wc-cart' );
-      wp_dequeue_script( 'wc-chosen' );
-      wp_dequeue_script( 'woocommerce' );
-      wp_dequeue_script( 'prettyPhoto' );
-      wp_dequeue_script( 'prettyPhoto-init' );
-      wp_dequeue_script( 'jquery-blockui' );
-      wp_dequeue_script( 'jquery-placeholder' );
-      wp_dequeue_script( 'fancybox' );
-      wp_dequeue_script( 'jqueryui' );
-    }
-  }
-}, 99);
+//       // Remove unnecessary scripts.
+//       wp_dequeue_script( 'wc_price_slider' );
+//       wp_dequeue_script( 'wc-single-product' );
+//       wp_dequeue_script( 'wc-add-to-cart' );
+//       wp_dequeue_script( 'wc-cart-fragments' );
+//       wp_dequeue_script( 'wc-checkout' );
+//       wp_dequeue_script( 'wc-add-to-cart-variation' );
+//       wp_dequeue_script( 'wc-single-product' );
+//       wp_dequeue_script( 'wc-cart' );
+//       wp_dequeue_script( 'wc-chosen' );
+//       wp_dequeue_script( 'woocommerce' );
+//       wp_dequeue_script( 'prettyPhoto' );
+//       wp_dequeue_script( 'prettyPhoto-init' );
+//       wp_dequeue_script( 'jquery-blockui' );
+//       wp_dequeue_script( 'jquery-placeholder' );
+//       wp_dequeue_script( 'fancybox' );
+//       wp_dequeue_script( 'jqueryui' );
+//     }
+//   }
+// }, 99);
