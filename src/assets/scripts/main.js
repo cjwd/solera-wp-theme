3d0cc40ad0a7349a1f574d311de32234e422c9f4
cjwd <chinarakinda@gmail.com>
2020-04-09 07:50:33 -0400

    Add a conditional check for products so it wouldn't throw an error

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -10,25 +10,27 @@ btnSearch.addEventListener("click", function(e) {
 
 /** Add To Cart Stepper */
 const productsEl = document.querySelector(".products");
-productsEl.addEventListener("click", function(e) {
-  let buttonEl = e.target,
-    action = e.target.dataset.action;
+if (productsEl) {
+  productsEl.addEventListener("click", function(e) {
+    let buttonEl = e.target,
+      action = e.target.dataset.action;
 
-  if (action === "add") {
-    add(buttonEl);
-  }
+    if (action === "add") {
+      add(buttonEl);
+    }
 
-  if (action === "minus") {
-    minus(buttonEl);
-  }
-});
+    if (action === "minus") {
+      minus(buttonEl);
+    }
+  });
 
-productsEl.addEventListener("change", function(e) {
-  if (e.target.matches(".qty")) {
-    setStepperState(
-      Number(e.target.value),
-      e.target.parentElement.previousElementSibling,
-    );
-  }
-});
+  productsEl.addEventListener("change", function(e) {
+    if (e.target.matches(".qty")) {
+      setStepperState(
+        Number(e.target.value),
+        e.target.parentElement.previousElementSibling,
+      );
+    }
+  });
+}
 /** Add To Cart Stepper End */
