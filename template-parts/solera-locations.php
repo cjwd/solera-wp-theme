73d474f45623dc231d285e84777d1a9ac3d9cd33
cjwd <chinarakinda@gmail.com>
2020-05-08 09:13:53 -0400

    Move locations widget to its own template to cut down on repetition in header file

---
+++ template-parts/solera-locations.php
@@ -0,0 +1,8 @@
+<div class="c-soleraLocations">
+  <div class="c-soleraLocation c-soleraLocation--north">
+    <a href="/contact-us-north">NORTH <i class="fas fa-map-marker-alt"></i> <span class="c-soleraLocation__title">CORNER OF TRAGARETE & GREY STREET</span></a>
+  </div>
+  <div class="c-soleraLocation c-soleraLocation--south">
+    <a href="/contact-us-north">SOUTH <i class="fas fa-map-marker-alt"></i> <span class="c-soleraLocation__title">C3 CENTRE COURTYARD</span></a>
+  </div>
+</div>
