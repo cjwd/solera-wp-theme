2d63083dd611abbedcfb384f6ad9d0d7e387ef52
cjwd <chinarakinda@gmail.com>
2020-05-13 13:29:54 -0400

    Fix url for south contact page

--- template-parts/solera-locations.php
+++ template-parts/solera-locations.php
@@ -3,6 +3,6 @@
     <a href="/contact-us-north">NORTH <i class="fas fa-map-marker-alt"></i> <span class="c-soleraLocation__title">CORNER OF TRAGARETE & GREY STREET</span></a>
   </div>
   <div class="c-soleraLocation c-soleraLocation--south">
-    <a href="/contact-us-north">SOUTH <i class="fas fa-map-marker-alt"></i> <span class="c-soleraLocation__title">C3 CENTRE COURTYARD</span></a>
+    <a href="/contact-us-south">SOUTH <i class="fas fa-map-marker-alt"></i> <span class="c-soleraLocation__title">C3 CENTRE COURTYARD</span></a>
   </div>
 </div>
