dbddc277cebecf70e116aea6bc3e25c16bb772c2
cjwd <chinarakinda@gmail.com>
2020-05-13 13:39:39 -0400

    Fix typo

--- template-parts/solera-locations.php
+++ template-parts/solera-locations.php
@@ -1,6 +1,6 @@
 <div class="c-soleraLocations">
   <div class="c-soleraLocation c-soleraLocation--north">
-    <a href="/contact-us-north">NORTH <i class="fas fa-map-marker-alt"></i> <span class="c-soleraLocation__title">CORNER OF TRAGARETE & GREY STREET</span></a>
+    <a href="/contact-us-north">NORTH <i class="fas fa-map-marker-alt"></i> <span class="c-soleraLocation__title">CORNER OF TRAGARETE ROAD &amp; GRAY STREET</span></a>
   </div>
   <div class="c-soleraLocation c-soleraLocation--south">
     <a href="/contact-us-south">SOUTH <i class="fas fa-map-marker-alt"></i> <span class="c-soleraLocation__title">C3 CENTRE COURTYARD</span></a>
