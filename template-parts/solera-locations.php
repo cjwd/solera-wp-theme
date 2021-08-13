55c7b71130175eda9da949c2ab7e8b4f2cda9eae
Chinara James <cjwd@Chinaras-MacBook-Pro.local>
2021-08-13 12:24:04 -0400

    Update south location address

--- template-parts/solera-locations.php
+++ template-parts/solera-locations.php
@@ -3,6 +3,6 @@
     <a href="/contact-us-north">NORTH <i class="fas fa-map-marker-alt"></i> <span class="c-soleraLocation__title">CORNER OF TRAGARETE ROAD &amp; GRAY STREET</span></a>
   </div>
   <div class="c-soleraLocation c-soleraLocation--south">
-    <a href="/contact-us-south">SOUTH <i class="fas fa-map-marker-alt"></i> <span class="c-soleraLocation__title">C3 CENTRE COURTYARD</span></a>
+    <a href="/contact-us-south">SOUTH <i class="fas fa-map-marker-alt"></i> <span class="c-soleraLocation__title">GOPAUL LANDS, MARABELLA</span></a>
   </div>
 </div>
