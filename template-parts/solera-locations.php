39471817304fb0a6cbf5209536e4d19f790ff94a
Chinara James <chinarakinda@gmail.com>
2021-12-15 14:30:22 -0400

    Fix typo

--- template-parts/solera-locations.php
+++ template-parts/solera-locations.php
@@ -6,6 +6,6 @@
     <a href="/contact-us-south">SOUTH <i class="fas fa-map-marker-alt"></i> <span class="c-soleraLocation__title">GOPAUL LANDS, MARABELLA</span></a>
   </div>
   <div class="c-soleraLocation c-soleraLocation--east">
-    <a href="/contact-us-EAST">EAST <i class="fas fa-map-marker-alt"></i> <span class="c-soleraLocation__title">91 EASTERN MAIN ROAD, PORT OF SPAIN</span></a>
+    <a href="/contact-us-east">EAST <i class="fas fa-map-marker-alt"></i> <span class="c-soleraLocation__title">91 EASTERN MAIN ROAD, PORT OF SPAIN</span></a>
   </div>
 </div>
